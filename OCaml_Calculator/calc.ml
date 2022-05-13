4c851e2e69f2a766c1742656ae1dd7c377016186
JulianH <you@example.com>
2022-05-13 17:40:37 +0200

    Ctrl+C sigint handler added.

--- OCaml_Calculator/calc.ml
+++ OCaml_Calculator/calc.ml
@@ -3,12 +3,18 @@
 (* Open used libs *)
 open Parser;;
 open Lexer;;
+open Sys;;
 
 (* Welcome message *)
-print_string ("\nHello! \n
-Do you want to calculate something? \nLet's start! \n
-Enter your wish for calculation after \"~~\" symbol. \n
-e.g. \n~~\t3 + 3 \nResult:\t6 \n\n\n");;
+print_char ('\n');
+print_string ("Hello! \n");;
+print_string ("Do you want to calculate something? \n");;
+print_string ("Let's start! \n");;
+print_string ("Enter your wish for calculation after \"~~\" symbol. \n");;
+print_string ("e.g. \n~~\t3 + 3 \n");;
+print_string ("Result:\t6 \n");;
+(* print_string ("Please separate operations and numbers with whitespaces. \n");; *)
+print_string ("To terminate this program use Ctrl+C.\n\n");;
 
 (* Looping flag *)
 let looping = ref true;;
@@ -18,6 +24,17 @@ let lexbuf = Lexing.from_channel stdin;;
 
 (* Print first prompt sign *)
 print_string("~~\t");; 
+
+(* Ctrl + C == leave the calc *)
+
+(* Exception for ending calc *)
+exception EndCalc;;
+
+(* Function to handle sigint (CTRL+C) *)
+let endCalc _ = raise EndCalc;;
+
+(* Sigint handler = endCalc *)
+signal sigint (Signal_handle endCalc);;
 
 (* Main loop *)
 while (!looping) do
@@ -32,15 +49,21 @@ while (!looping) do
               token lexbuf in
           (* Print out the result *)
           print_string("Result:\t"); 
-          print_int result; 
+          print_float result; 
           (* Flush output with prompt sign *)
           print_string("\n~~\t"); 
           flush stdout;
     (* Exception handler *)
     with 
+      (* Handle Ctrl+C *)
+      | EndCalc -> 
+              print_endline ("\n\tCtrl+C pressed. See you soon!");
+              looping := false
+      (* Handle end of file *)
       | Lexer.Eof ->
               print_endline ("End of file.");
               looping := false
+      (* Unexpected token in lexer *)
       | UNEXPECTED_TOKEN ->
               print_endline ("Unexpected token error.\n")
       (* Don't stop loop while debuging after unexpected (yet) error *)
